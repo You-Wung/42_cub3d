15727f993ee13fc1dce67f21610396778ecc0448
Tewung You <tyou@c7r4s3.42seoul.kr>
2021-04-24 16:49:00 +0900

    7

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: tyou <tyou@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/23 22:15:58 by tyou              #+#    #+#             */
-/*   Updated: 2021/04/24 16:03:24 by tyou             ###   ########.fr       */
+/*   Updated: 2021/04/24 16:48:20 by tyou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static void		check_screensize(t_info *info)
 		info->height = y - 1;
 }
 
-static int		set_gnl(t_info *info)
+static int		set_gnl(t_info *info, char *argv)
 {
 	int		fd;
 	char	*str;
@@ -32,7 +32,7 @@ static int		set_gnl(t_info *info)
 
 	info->floor = -1;
 	info->ceilling = -1;
-	fd = open("./map.cub", O_RDONLY);
+	fd = open(argv, O_RDONLY);
 	if (fd < 0)
 		free_exit(info, 1);
 	while (get_next_line(fd, &str))
@@ -71,7 +71,7 @@ int				main(int argc, char **argv)
 
 	ft_memset(&info, 0, sizeof(t_info));
 	info.mlx = mlx_init();
-	fd = set_gnl(&info);
+	fd = set_gnl(&info, argv[1]);
 	info.map = read_map(fd, &info);
 	set_up(&info);
 	info.win = mlx_new_window(info.mlx, info.width, info.height, "cub3d");
@@ -79,12 +79,12 @@ int				main(int argc, char **argv)
 	info.img.data = (int *)mlx_get_data_addr(info.img.img,
 							&info.img.bpp, &info.img.size_l, &info.img.endian);
 	mlx_loop_hook(info.mlx, &main_loop, &info);
-	if (argc == 2 && !ft_strcmp(argv[1], "--save"))
+	if (argc == 3 && !ft_strcmp(argv[2], "--save"))
 	{
 		main_loop(&info);
 		save_bmp(&info);
 	}
-	else if (argc == 2)
+	else if (argc >= 3)
 		exit(printf("Error\n"));
 	mlx_hook(info.win, X_EVENT_KEY_PRESS, 0, &key_press, &info);
 	mlx_hook(info.win, X_EVENT_KEY_RELEASE, 0, &key_release, &info);
